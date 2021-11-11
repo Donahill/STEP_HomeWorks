34ef8fc2f5fd04e7d9dbcac385f0b4acf1da00cc
Donahill <donahill78@gmail.com>
2021-11-11 23:37:39 +0200

    Пурумпумпум

--- ComplexDigit/ComplexDigit/Program.cs
+++ ComplexDigit/ComplexDigit/Program.cs
@@ -6,6 +6,20 @@ using System.Threading.Tasks;
 
 namespace ComplexDigit
 {
+    class inputI
+    {
+        public Complex INp()
+        {
+            Console.Write("Введите числитель=>_\b");
+            string str = Console.ReadLine();
+            int ch1 = Convert.ToInt32(str);
+            Console.Write("Введите знаменатель=>_\b");
+            string str1 = Console.ReadLine();
+            int zn1 = Convert.ToInt32(str);
+            Complex s = new Complex(ch1, zn1);
+            return s;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
