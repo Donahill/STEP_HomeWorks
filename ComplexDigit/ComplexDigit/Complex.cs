a1c13e0ddeaff6fed4198c640d6e8438e129c89b
Donahill <donahill78@gmail.com>
2021-11-21 22:02:56 +0200

    update

---
+++ ComplexDigit/ComplexDigit/Complex.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ComplexDigit
+{
+
+	class Complex
+	{
+		double m_re, m_im;
+
+		public Complex(double re = 1, double im = 0)
+		{
+			m_re = re;
+			m_im = im;
+		}
+
+		public static Complex operator +(Complex a, Complex b)
+		{
+			return new Complex(a.m_re + b.m_re, a.m_im + b.m_im);
+		}
+
+		public static Complex operator -(Complex a, Complex b)
+		{
+			return Complex(a.m_re - b.m_re, a.m_im - b.m_im);
+		}
+
+		public static Complex operator *(Complex a, Complex b)
+		{
+			return Complex(a.m_re * b.m_re - a.m_im * b.m_im, a.m_re * b.m_im - a.m_im * b.m_re);
+		}
+
+		public static Complex operator /(Complex a, Complex b)
+		{
+			return Complex((a.m_re * b.m_re + a.m_im * b.m_im) / (a.m_im * a.m_im + b.m_im * b.m_im), (a.m_im * b.m_re - a.m_re * b.m_im) / (b.m_re * b.m_re + b.m_im * b.m_im));
+		}
+	}
+}
